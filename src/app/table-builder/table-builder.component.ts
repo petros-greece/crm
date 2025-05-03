7b97ec941fae99c17d3af87be0debc65c2bfad6e
Masquerade <petros-greece@users.noreply.github.com>
2025-05-03 18:10:08 +0300

    can hide columns

--- src/app/table-builder/table-builder.component.ts
+++ src/app/table-builder/table-builder.component.ts
@@ -5,14 +5,17 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { XlsxService } from '../services/xlsx.service';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 @Directive({
   selector: '[appColumnTemplate]',
   standalone: true
 })
 export class ColumnTemplateDirective {
   @Input('appColumnTemplate') key!: string;
-  constructor(public template: TemplateRef<any>) {}
+  constructor(public template: TemplateRef<any>) { }
 }
 
 export interface TableColumn {
@@ -38,23 +41,49 @@ export interface TableConfig {
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule,
     MatTableModule,
     MatIconModule,
     MatPaginatorModule,
     MatSortModule,
     DatePipe,
-    MatIcon
+    MatIcon,
+    MatButtonModule,
+    MatMenuModule,
+    MatCheckboxModule
   ],
   template: `
     <div class="flex flex-row justify-end">
-      <button (click)="xlsxService.exportTableToExcel(dataSource.data)">
+
+      <button mat-button (click)="xlsxService.exportTableToExcel(dataSource.data)">
         EXCEL
         <mat-icon>download</mat-icon>
       </button>
-      <button (click)="xlsxService.exportTableToJson(dataSource.data)">
+      <button mat-button (click)="xlsxService.exportTableToJson(dataSource.data)">
         JSON
         <mat-icon>download</mat-icon>
       </button>
+
+      <button mat-button [matMenuTriggerFor]="columnsMenu" #menuTrigger="matMenuTrigger">
+        <mat-icon class="m-auto">filter_list</mat-icon>
+      </button>
+      <mat-menu #columnsMenu="matMenu" class="column-menu">
+        <div class="p-2 flex flex-col gap-2" (click)="$event.stopPropagation()">
+          <div *ngFor="let col of config.columns" class="menu-item">
+            <mat-checkbox 
+              [checked]="columnVisibility[col.key]"
+              (change)="columnVisibility[col.key] = $event.checked; toggleColumn(col.key)"
+              color="primary">
+              {{ col.label }}
+            </mat-checkbox>
+          </div>
+          <button mat-stroked-button 
+                  class="mt-2 self-end"
+                  (click)="menuTrigger.closeMenu()">
+            Close
+          </button>
+        </div>
+      </mat-menu>
     </div>
     <div class="overflow-x-scroll">
       <table mat-table [dataSource]="dataSource" matSort class="overflow-scroll">
@@ -98,7 +127,7 @@ export interface TableConfig {
   styles: [`
     table { width: 100%; }
     mat-paginator { margin-top: 1rem; }
-    mat-icon { vertical-align: middle; }
+    table mat-icon { vertical-align: middle; }
   `]
 })
 export class TableBuilderComponent implements OnInit, AfterViewInit, OnChanges {
@@ -112,20 +141,19 @@ export class TableBuilderComponent implements OnInit, AfterViewInit, OnChanges {
   dataSource = new MatTableDataSource<any>();
   displayedColumns: string[] = [];
   templateMap = new Map<string, TemplateRef<any>>();
-
-  constructor(private cdr: ChangeDetectorRef) {}
+  columnVisibility: { [key: string]: boolean } = {};
+  constructor(private cdr: ChangeDetectorRef) { }
 
   ngOnInit() {
-    // initialize columns and data on first load
-    this.displayedColumns = this.config.columns.map(c => c.key);
+    this.initializeColumnVisibility();
+    this.updateDisplayedColumns();
     this.dataSource.data = this.config.data;
   }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['config'] && this.config) {
-      // update columns if changed
-      this.displayedColumns = this.config.columns.map(c => c.key);
-      // update data
+      this.initializeColumnVisibility();
+      this.updateDisplayedColumns();
       this.dataSource.data = this.config.data;
       // re-assign paginator and sort after view init
       if (this.paginator) this.dataSource.paginator = this.paginator;
@@ -150,4 +178,32 @@ export class TableBuilderComponent implements OnInit, AfterViewInit, OnChanges {
   getCustomTemplate(key: string): TemplateRef<any> | undefined {
     return this.templateMap.get(key);
   }
+
+
+  initializeColumnVisibility() {
+    // Initialize all columns as visible
+    this.columnVisibility = this.config.columns.reduce((acc, col) => {
+      acc[col.key] = true;
+      return acc;
+    }, {} as { [key: string]: boolean });
+  }
+
+  toggleColumn(columnKey: string) {
+    // No need to toggle here since we're using the event value
+    this.updateDisplayedColumns();
+  }
+  updateDisplayedColumns(initial = false) {
+    // Create new array reference to trigger change detection
+    this.displayedColumns = this.config.columns
+      .filter(col => this.columnVisibility[col.key])
+      .map(col => col.key);
+
+    // Force table to update
+    if (!initial) {
+      this.dataSource.data = [...this.dataSource.data];
+      this.cdr.detectChanges();
+    }
+  }
+
+
 }
