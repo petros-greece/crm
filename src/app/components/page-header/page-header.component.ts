212cb658f0ffbe3cc7dfed9fefabda4db1513fdf
Masquerade <petros-greece@users.noreply.github.com>
2025-05-13 13:35:31 +0300

    init page-header

---
+++ src/app/components/page-header/page-header.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-page-header',
+  standalone: true,
+  imports: [MatIcon, NgIf],
+  template: `
+    <div class="flex flex-row items-center justify-between px-3 py-3 bg-violet-800 rounded-lg shadow-md mb-4">
+      <h2 class="text-lg flex items-center gap-2 text-white">
+        {{ header }}
+        <mat-icon *ngIf="icon">{{ icon }}</mat-icon>
+      </h2>
+      <ng-content></ng-content>
+    </div>
+  `,
+  styleUrl: './page-header.component.scss'
+})
+export class PageHeaderComponent {
+  @Input() header: string = '';
+  @Input() icon?: string;
+}
