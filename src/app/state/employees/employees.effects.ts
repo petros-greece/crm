a460a44244e8604ec1197496321688a017307e9d
Masquerade <petros-greece@users.noreply.github.com>
2025-05-30 00:32:41 +0300

    minor

--- src/app/state/employees/employees.effects.ts
+++ src/app/state/employees/employees.effects.ts
@@ -1,22 +1,27 @@
-// employees.effects.ts
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { DataService } from '../../services/data.service';
 import { EmployeesActions } from './employees.actions';
-import { catchError, map, switchMap, of } from 'rxjs';
+import { DataService } from '../../services/data.service';
+import { Store } from '@ngrx/store';
+import { selectEmployees } from './employees.selectors';
+import { of } from 'rxjs';
+import { switchMap, map, catchError, withLatestFrom, filter } from 'rxjs/operators';
 
 @Injectable()
 export class EmployeesEffects {
   private actions$ = inject(Actions);
+  private store = inject(Store);
   private dataService = inject(DataService);
 
   loadEmployees$ = createEffect(() =>
     this.actions$.pipe(
       ofType(EmployeesActions.loadEmployees),
+      withLatestFrom(this.store.select(selectEmployees)),
+      filter(([_, employees]) => employees.length === 0), // skip if already loaded
       switchMap(() =>
         this.dataService.getEmployees().pipe(
-          map(employees => EmployeesActions.loadEmployeesSuccess({ employees })),
-          catchError(error => of(EmployeesActions.loadEmployeesFailure({ error })))
+          map((employees) => EmployeesActions.loadEmployeesSuccess({ employees })),
+          catchError((error) => of(EmployeesActions.loadEmployeesFailure({ error })))
         )
       )
     )
